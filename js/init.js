b09f9cc30e69a7d879957fde8f9ddd70b1fd30fb
szyth <syedzia.pro@gmail.com>
2020-06-30 13:28:45 +0530

    added image slider

--- js/init.js
+++ js/init.js
@@ -5,3 +5,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(".dropdown-trigger").dropdown();
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+});
+
+// setInterval(function() {
+//   $('.carousel.carousel-slider').carousel('next');
+// }, 2000); 
